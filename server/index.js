4b0b3940ea65f42914b48ae41d197f2058bb31ee
Alex <aberman12@yahoo.com>
2018-09-24 15:42:08 -0700

    ready to deploy. just need to make flexible css

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 var app = express();
-var port = 3000;
+// var port = 3000;
 var router = require("./router.js");
 var session = require("express-session");
 
+app.set("port", process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/../react-client/dist"));
@@ -24,6 +25,6 @@ app.use(
 
 app.use("/api", router);
 
-app.listen(port, function() {
-  console.log("successfully connected on port: ", port);
+app.listen(app.get("port"), function() {
+  console.log("successfully connected on port: ", 3000);
 });
