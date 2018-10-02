64fb80284bca32e1e6016c109ae637c0a1a9e08f
Alex <aberman12@yahoo.com>
2018-10-02 12:06:37 -0700

    done

--- database-mongoDB/index.js
+++ database-mongoDB/index.js
@@ -2,11 +2,11 @@ var mongoose = require("mongoose");
 var bcrypt = require("bcrypt");
 const saltRounds = 10;
 let currentPassword;
-const connector =
-  "mongodb://alex.k.berman@gmail.com:Fender23@ds113853.mlab.com:13853/heroku_6lz5cvzc";
 // mongodb://localhost/spacew
 
-mongoose.connect(connector);
+mongoose.connect(
+  "mongodb://aberman:Fender23@ds113853.mlab.com:13853/heroku_6lz5cvzc"
+);
 
 var db = mongoose.connection;
 
